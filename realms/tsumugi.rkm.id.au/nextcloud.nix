0c4e6ae38cfee3e5a30d835cb522f9001378bc6e
Ruben Maher <r@rkm.id.au>
2017-08-06 19:54:41 +0930

    Fix nextcloud

--- realms/tsumugi.rkm.id.au/nextcloud.nix
+++ realms/tsumugi.rkm.id.au/nextcloud.nix
@@ -13,35 +13,37 @@ in {
     # sslCertificate = "/var/lib/acme/${hostname}/fullchain.pem";
     # sslCertificateKey = "/var/lib/acme/${hostname}/key.pem";
     extraConfig = ''
-      # Path to the root of your installation
-      root ${pkgs.nextcloud};
-      # set max upload size
-      client_max_body_size 10G;
-      fastcgi_buffers 64 4K;
-
-      # Disable gzip to avoid the removal of the ETag header
-      gzip off;
-
-      # Uncomment if your server is build with the ngx_pagespeed module
-      # This module is currently not supported.
-      #pagespeed off;
-
-      index index.php;
-      error_page 403 /core/templates/403.php;
-      error_page 404 /core/templates/404.php;
-      add_header Strict-Transport-Security "max-age=15768000; includeSubDomains; preload;";
+      # Add headers to serve security related headers
+      # Before enabling Strict-Transport-Security headers please read into this
+      # topic first.
+      # add_header Strict-Transport-Security "max-age=15768000;
+      # includeSubDomains; preload;";
+      #
+      # WARNING: Only add the preload option once you read about
+      # the consequences in https://hstspreload.org/. This option
+      # will add the domain to a hardcoded list that is shipped
+      # in all major browsers and getting removed from this list
+      # could take several months.
       add_header X-Content-Type-Options nosniff;
-      add_header X-Frame-Options "SAMEORIGIN";
       add_header X-XSS-Protection "1; mode=block";
       add_header X-Robots-Tag none;
       add_header X-Download-Options noopen;
       add_header X-Permitted-Cross-Domain-Policies none;
+
+      # Path to the root of your installation
+      root ${pkgs.nextcloud};
 
       location = /robots.txt {
         allow all;
         log_not_found off;
         access_log off;
       }
+
+      # The following 2 rules are only needed for the user_webfinger app.
+      # Uncomment it if you're planning to use this app.
+      #rewrite ^/.well-known/host-meta /public.php?service=host-meta last;
+      #rewrite ^/.well-known/host-meta.json /public.php?service=host-meta-json
+      # last;
 
       location = /.well-known/carddav {
         return 301 $scheme://$host/remote.php/dav;
@@ -51,9 +53,24 @@ in {
         return 301 $scheme://$host/remote.php/dav;
       }
 
+      # set max upload size
+      client_max_body_size 10G;
+      fastcgi_buffers 64 4K;
+
+      # Enable gzip but do not remove ETag headers
+      gzip on;
+      gzip_vary on;
+      gzip_comp_level 4;
+      gzip_min_length 256;
+      gzip_proxied expired no-cache no-store private no_last_modified no_etag auth;
+      gzip_types application/atom+xml application/javascript application/json application/ld+json application/manifest+json application/rss+xml application/vnd.geo+json application/vnd.ms-fontobject application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml font/opentype image/bmp image/svg+xml image/x-icon text/cache-manifest text/css text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/x-cross-domain-policy;
+
+      # Uncomment if your server is build with the ngx_pagespeed module
+      # This module is currently not supported.
+      #pagespeed off;
+
       location / {
-        rewrite ^(/core/doc/[^\/]+/)$ $1/index.html;
-        try_files $uri $uri/ /index.php;
+        rewrite ^ /index.php$uri;
       }
 
       location ~ ^/(?:build|tests|config|lib|3rdparty|templates|data)/ {
@@ -65,36 +82,62 @@ in {
       }
 
       location ~ ^/(?:index|remote|public|cron|core/ajax/update|status|ocs/v[12]|updater/.+|ocs-provider/.+)\.php(?:$|/) {
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_split_path_info ^(.+\.php)(/.*)$;
         include ${pkgs.nginx}/conf/fastcgi_params;
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         fastcgi_param PATH_INFO $fastcgi_path_info;
         fastcgi_param HTTPS on;
+        #Avoid sending the security headers twice
+        fastcgi_param modHeadersAvailable true;
+        fastcgi_param front_controller_active true;
         fastcgi_pass unix:/run/phpfpm/nextcloud;
         fastcgi_intercept_errors on;
+        fastcgi_request_buffering off;
+      }
+
+      location ~ ^/(?:updater|ocs-provider)(?:$|/) {
+        try_files $uri/ =404;
+        index index.php;
       }
 
       # Adding the cache control header for js and css files
-      # Make sure it is BELOW the location ~ \.php(?:$|/) { block
-      location ~* \.(?:css|js)$ {
-        add_header Cache-Control "public, max-age=7200";
-        # Add headers to serve security related headers
-        add_header Strict-Transport-Security "max-age=15768000; includeSubDomains; preload;";
+      # Make sure it is BELOW the PHP block
+      location ~ \.(?:css|js|woff|svg|gif)$ {
+        try_files $uri /index.php$uri$is_args$args;
+        add_header Cache-Control "public, max-age=15778463";
+        # Add headers to serve security related headers (It is intended to
+        # have those duplicated to the ones above)
+        # Before enabling Strict-Transport-Security headers please read into
+        # this topic first.
+        # add_header Strict-Transport-Security "max-age=15768000;
+        #  includeSubDomains; preload;";
+        #
+        # WARNING: Only add the preload option once you read about
+        # the consequences in https://hstspreload.org/. This option
+        # will add the domain to a hardcoded list that is shipped
+        # in all major browsers and getting removed from this list
+        # could take several months.
         add_header X-Content-Type-Options nosniff;
-        add_header X-Frame-Options "SAMEORIGIN";
         add_header X-XSS-Protection "1; mode=block";
         add_header X-Robots-Tag none;
+        add_header X-Download-Options noopen;
+        add_header X-Permitted-Cross-Domain-Policies none;
         # Optional: Don't log access to assets
         access_log off;
       }
 
-      # Optional: Don't log access to other assets
-      location ~* \.(?:jpg|jpeg|gif|bmp|ico|png|swf)$ {
+      location ~ \.(?:png|html|ttf|ico|jpg|jpeg)$ {
+        try_files $uri /index.php$uri$is_args$args;
+        # Optional: Don't log access to other assets
         access_log off;
       }
 
-      location ^~ /apps {
-        alias /var/lib/nextcloud/apps;
+      location ^~ /apps_addon {
+        alias ${nextcloudPath}/apps;
+      }
+
+      location ^~ /apps_internal {
+        alias ${pkgs.nextcloud}/apps;
       }
     '';
   };
