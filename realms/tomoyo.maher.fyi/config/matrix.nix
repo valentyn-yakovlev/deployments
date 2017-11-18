6f1d9a8f510a2e91c8dfaaca4cd5dadc459487b4
Ruben Maher <r@rkm.id.au>
2017-11-19 08:17:45 +1030

    Add matrix thing

---
+++ realms/tomoyo.maher.fyi/config/matrix.nix
@@ -0,0 +1,71 @@
+{}:
+let
+  secrets = import ./secrets.nix;
+
+  fqdn = "maher.fyi";
+in {
+  networking.firewall = {
+    allowedTCPPorts = [
+      3478 # coturn
+      8448 # matrix
+    ];
+    allowedUDPPorts = [
+      3478 # coturn
+    ];
+    allowedUDPPortRanges = [
+      { from = 49152; to = 65535; } # coturn
+    ];
+  };
+
+  services.matrix-synapse = {
+    allow_guest_access = false;
+    bcrypt_rounds = "12";
+    enable = true;
+    web_client = false;
+    enable_registration = false;
+    registration_shared_secret = secrets.services.matrix-synapse.registration_shared_secret;
+    server_name = fqdn;
+    database_type = "psycopg2";
+    database_args = {
+      user = "synapse";
+      password = secrets.services.matrix-synapse.database_args.password;
+      database = "matrix-synapse";
+      host = "localhost";
+      cp_min = "5";
+      cp_max = "10";
+    };
+    turn_uris = [
+      "turn:${fqdn}:3478?transport=udp"
+      "turn:${fqdn}:3478?transport=tcp"
+    ];
+    # This needs to be the same as services.coturn.static-auth-secret
+    turn_shared_secret = secrets.services.matrix-synapse.turn_shared_secret;
+    turn_user_lifetime = "24h";
+    url_preview_enabled = true;
+  };
+
+  services.coturn = {
+    enable = true;
+    lt-cred-mech = true;
+    static-auth-secret = secrets.services.coturn.static-auth-secret;
+    realm = fqdn;
+    cert = "/var/lib/acme/${fqdn}/fullchain.pem";
+    pkey = "/var/lib/acme/${fqdn}/key.pem";
+    min-port = 49152;
+    max-port = 65535;
+  };
+
+  services.postgresql.enable = true;
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."${fqdn}" = {
+      serverName = fqdn;
+      forceSSL = true;
+      enableACME = true;
+      acmeRoot = "/var/lib/acme/acme-challenge";
+    };
+  };
+
+  security.acme.certs."${fqdn}".postRun = "systemctl reload-or-restart matrix-synapse coturn";
+}
